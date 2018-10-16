38ab20baea92e83d22dd930c39b2c8f1236b3f5a
RaquelGlez <gonzalez.c.raquel@gmail.com>
2018-10-16 18:53:33 -0500

    Updates

---
+++ precache-manifest.315c84a0ec384a5be59c0b18f920ed0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/proyecto-a/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "d73f056879e21a2db405",
+    "url": "/proyecto-a/static/js/runtime~main.d73f0568.js"
+  },
+  {
+    "revision": "c73731435a003bf931bd",
+    "url": "/proyecto-a/static/js/main.c7373143.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/proyecto-a/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "c73731435a003bf931bd",
+    "url": "/proyecto-a/static/css/main.9101ac88.chunk.css"
+  },
+  {
+    "revision": "1b4c8dd344bf56d00f928be290084862",
+    "url": "/proyecto-a/index.html"
+  }
+];
